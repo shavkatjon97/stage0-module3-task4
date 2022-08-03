30bf831f507c76ccffec388bc4ad0d87edf5b6bc
Shavkatjon97 <dadshavkatjon97@gmail.com>
2022-08-03 22:48:15 +0500

    Task4 was done

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char m=99;
+        char m1=104;
+        char m2=97;
+        char m3=114;
+        System.out.println(m+""+m1+""+m2+""+m3);
     }
 }
